7773c13d80b723280e390dd1726e01e252abcd49
Prabhat Thapa <prabhat.thapachh@gmail.com>
2020-07-04 16:38:58 +0545

    forgot to remove dd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,6 @@
         </style>
     </head>
     <body>
-    {{ dd($_SERVER)}}
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
